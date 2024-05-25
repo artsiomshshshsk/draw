4f761e78056ca4ba9b8f9199b5505f16df9ee72e
artsiomshshshsk <artsiomshablinskiy@gmail.com>
2024-05-26 01:48:55 +0200

    project bootstrap

---
+++ backend/draw/src/test/java/com/github/artsiomshshshsk/draw/DrawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.artsiomshshshsk.draw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

52064b4b7f9583be6f292d30f66d1a821b14d644
Vera Yemialyanava <269565@student.pwr.edu.pl>
2024-06-18 13:30:50 +0200

    basic pan and zoom functions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import {getBoard} from '@/api/ApiClient.ts';
+import {generateId, getBoard} from '@/api/ApiClient.ts';
 import {Input} from '@/components/ui/input.tsx';
 import {DrawElement, DrawEvent} from '@/domain.ts';
 import {useWebSocket} from '@/hooks/useWebSocket.ts';
@@ -9,18 +9,80 @@ import ToolBar from "@/components/ToolBar.tsx";
 import useAction from "@/hooks/useAction.ts";
 import {updateRoughElement, createRoughElement} from "@/elementFactory.ts";
 import {getElementAtPosition} from "@/lib/utils.ts";
+import ActionBar from "@/components/ActionBar.tsx";
+import usePressedKeys from "@/hooks/usePressedKeys.ts";
 
 function App() {
 
     const canvasRef = useRef<HTMLCanvasElement>(null);
-    const { action, setAction, elements, setElements, tool, setTool, addNewElement, updateElement, selectedElementId, startMovingElement, moveElement, stopMovingElement } = useAction('LINE');
+    const {
+        action,
+        setAction,
+        elements,
+        setElements,
+        tool,
+        setTool,
+        addNewElement,
+        updateElement,
+        selectedElementId,
+        startMovingElement,
+        moveElement,
+        stopMovingElement
+    } = useAction('LINE');
     const [username,] = useState<string>(`user-${Math.floor(Math.random() * 1000)}`);
     const [room,] = useState<string | undefined>('artsiRoom');
+
+    const [scale, setScale] = useState<number>(1);
+    const [panOffset, setPanOffset] = useState<{ x: number; y: number }>({x: 110, y: 110});
+    const [scaleOffset, setScaleOffset] = useState<{ x: number; y: number }>({x: 0, y: 0});
+    const pressedKeys = usePressedKeys();
+
+    const onZoom = (delta: number) => {
+        setScale((prevScale) => Math.min(20, Math.max(0.1, prevScale + delta)));
+    }
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        if (!canvas) return;
+
+        const zoomFunction = (event: WheelEvent) => {
+            if (event.ctrlKey) {
+                event.preventDefault();
+                onZoom(event.deltaY * -0.01);
+            }
+        };
+        canvas.addEventListener('wheel', zoomFunction);
+
+        return () => {
+            canvas.removeEventListener('wheel', zoomFunction);
+        };
+    }, [pressedKeys]);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        if (!canvas) return;
+
+        const panFunction = (event: WheelEvent) => {
+            if (!event.ctrlKey) {
+                event.preventDefault();
+                setPanOffset((prevOffset) => ({
+                    x: prevOffset.x - event.deltaX,
+                    y: prevOffset.y - event.deltaY
+                }));
+            }
+        };
+        canvas.addEventListener('wheel', panFunction);
+
+        return () => {
+            canvas.removeEventListener('wheel', panFunction);
+        };
+    }, []);
+
 
     useEffect(() => {
         getBoard().then((board: DrawElement[]) => {
             setElements(board.map(element => {
-                return { ...element, roughElement: updateRoughElement(element) };
+                return {...element, roughElement: updateRoughElement(element)};
             }));
         });
     }, []);
@@ -34,7 +96,7 @@ function App() {
             if (event.userId == username) return;
 
             if (event.type === 'CREATE') {
-                const newElement = { ...event.element, roughElement: updateRoughElement(event.element) };
+                const newElement = {...event.element, roughElement: updateRoughElement(event.element)};
                 addNewElement(newElement);
             }
 
@@ -44,25 +106,32 @@ function App() {
         }
     });
 
+    const getMouseCoordinates = (event: React.MouseEvent<HTMLCanvasElement>) => {
+        const canvas = canvasRef.current!;
+        const clientX = (event.clientX - panOffset.x * scale + scaleOffset.x) / scale;
+        const clientY = (event.clientY - panOffset.y * scale + scaleOffset.y) / scale;
+        return {clientX, clientY};
+    }
+
     const handleMouseDown = (event: React.MouseEvent<HTMLCanvasElement>) => {
         if (!username) return;
 
-        const { clientX, clientY } = event;
+        const {clientX, clientY} = getMouseCoordinates(event)
         if (tool === 'SELECT') {
             const clickedElement = getElementAtPosition(clientX, clientY, elements);
             if (clickedElement) {
-                setAction({ action: 'SELECTION', elementId: clickedElement.id });
+                setAction({action: 'SELECTION', elementId: clickedElement.id});
                 startMovingElement(clickedElement.id, clientX, clientY);
                 return;
             }
         } else {
             createRoughElement(clientX, clientY, clientX, clientY, tool).then((createdElement) => {
-                setAction({ action: 'DRAWING', elementId: createdElement.id });
+                setAction({action: 'DRAWING', elementId: createdElement.id});
                 addNewElement(createdElement);
 
                 sendDrawEvent(
                     `/app/draw/${room}`,
-                    JSON.stringify({ element: createdElement, type: 'CREATE', userId: username })
+                    JSON.stringify({element: createdElement, type: 'CREATE', userId: username})
                 );
             });
         }
@@ -73,18 +142,18 @@ function App() {
 
         if (action.action === 'SELECTION') {
             stopMovingElement();
-            setAction({ action: 'NONE', elementId: undefined });
+            setAction({action: 'NONE', elementId: undefined});
         }
 
         if (action.action === 'DRAWING') {
-            setAction({ action: 'NONE', elementId: undefined });
+            setAction({action: 'NONE', elementId: undefined});
         }
     };
 
-    const handleMouseMove = (event: React.MouseEvent<HTMLElement>) => {
+    const handleMouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
         if (!username) return;
 
-        const { clientX, clientY } = event;
+        const {clientX, clientY} = getMouseCoordinates(event)
         const canvas = canvasRef.current;
 
         if (action.action === 'SELECTION' && selectedElementId !== null) {
@@ -93,7 +162,7 @@ function App() {
             if (element) {
                 sendDrawEvent(
                     `/app/draw/${room}`,
-                    JSON.stringify({ element, type: 'UPDATE', userId: username })
+                    JSON.stringify({element, type: 'UPDATE', userId: username})
                 );
             }
             return;
@@ -103,10 +172,15 @@ function App() {
             setElements((prevState) => {
                 return prevState.map((element) => {
                     if (element.id === action.elementId) {
-                        const updatedElement = { ...element, x2: clientX, y2: clientY, roughElement: updateRoughElement({ ...element, x2: clientX, y2: clientY }) };
+                        const updatedElement = {
+                            ...element,
+                            x2: clientX,
+                            y2: clientY,
+                            roughElement: updateRoughElement({...element, x2: clientX, y2: clientY})
+                        };
                         sendDrawEvent(
                             `/app/draw/${room}`,
-                            JSON.stringify({ element: updatedElement, type: 'UPDATE', userId: username })
+                            JSON.stringify({element: updatedElement, type: 'UPDATE', userId: username})
                         );
                         return updatedElement;
                     }
@@ -127,12 +201,20 @@ function App() {
         const rc = rough.canvas(canvas);
 
         context.clearRect(0, 0, canvas.width, canvas.height);
+        const scaledWidth = canvas.width * scale;
+        const scaledHeight = canvas.height * scale;
+        const scaledOffsetX = (scaledWidth - canvas.width) / 2;
+        const scaledOffsetY = (scaledHeight - canvas.height) / 2;
+        setScaleOffset({x: scaledOffsetX, y: scaledOffsetY})
         context.save();
 
-        elements.forEach(({ roughElement }) => rc.draw(roughElement));
+        context.translate(panOffset.x * scale - scaledOffsetX, panOffset.y * scale - scaledOffsetY);
+        context.scale(scale, scale);
+
+        elements.forEach(({roughElement}) => rc.draw(roughElement));
         context.restore();
 
-    }, [elements]);
+    }, [elements, scale, panOffset]);
 
     return (
         <div>
@@ -141,6 +223,7 @@ function App() {
                 <Input className={"w-24 m-4"} value={username} disabled={true}/>
             </div>
             <ToolBar tool={tool} setTool={setTool}/>
+            <ActionBar scale={scale} setScale={setScale} onZoom={onZoom}/>
             <canvas
                 ref={canvasRef}
                 width={window.innerWidth}
@@ -148,7 +231,7 @@ function App() {
                 onMouseDown={handleMouseDown}
                 onMouseMove={handleMouseMove}
                 onMouseUp={handleMouseUp}
-                className={'absolute bg-white'}
+                className={'absolute bg-white z-1'}
             >
             </canvas>
         </div>
--- frontend/src/components/ToolBar.tsx
+++ frontend/src/components/ToolBar.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {ToggleGroup, ToggleGroupItem} from "@/components/ui/toggle-group"
-import {FaSquare, FaMinus, FaCircle} from 'react-icons/fa';
-import {FaArrowPointer} from 'react-icons/fa6';
+import {FaSquare, FaMinus, FaCircle, FaArrowPointer} from 'react-icons/fa6';
 import {DrawElementType} from "@/domain.ts";
 
 interface ToolbarProps {
@@ -16,7 +15,7 @@ const Toolbar: React.FC<ToolbarProps> = ({tool, setTool}) => {
     }
 
     return (
-        <ToggleGroup className="fixed inset-x-2/4 z-10 p-2 m-2" type="single" value={tool} aria-label="Drawing-tools" onValueChange={handleToolChange}>
+        <ToggleGroup className="flex flex-row fixed inset-x-2/4 z-10 p-2 m-2" type="single" value={tool} aria-label="Drawing-tools" onValueChange={handleToolChange}>
             <ToggleGroupItem value="SELECT" aria-label="Select">
                 <FaArrowPointer/>
             </ToggleGroupItem>

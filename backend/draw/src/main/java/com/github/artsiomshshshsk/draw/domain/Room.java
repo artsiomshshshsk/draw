ada514e1c567ccffc584e2ed9a21b56f6ae112ca
artsiomshshshsk <artsiomshablinskiy@gmail.com>
2024-06-21 09:44:44 +0200

    mongo

--- backend/draw/src/main/java/com/github/artsiomshshshsk/draw/domain/Room.java
+++ backend/draw/src/main/java/com/github/artsiomshshshsk/draw/domain/Room.java
@@ -1,8 +1,13 @@
 package com.github.artsiomshshshsk.draw.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.util.Map;
 
+@Document(collection = "rooms")
 public record Room(
+        @Id
         String roomId,
         Map<Integer, DrawElement> board
 ){}
--- backend/draw/src/main/java/com/github/artsiomshshshsk/draw/repository/RoomRepository.java
+++ backend/draw/src/main/java/com/github/artsiomshshshsk/draw/repository/RoomRepository.java
@@ -1,6 +1,5 @@
 package com.github.artsiomshshshsk.draw.repository;
 
-import com.github.artsiomshshshsk.draw.DrawController;
 import com.github.artsiomshshshsk.draw.domain.DrawElement;
 import com.github.artsiomshshshsk.draw.domain.Room;
 
